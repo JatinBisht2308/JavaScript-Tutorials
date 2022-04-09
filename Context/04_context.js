554046bf208ed598643c9fad4a91d0e571dfeb8b
JatinBisht2308 <jatinbisht2308@gmail.com>
2022-04-09 11:21:24 +0530

    :fire:Context added

---
+++ Context/04_context.js
@@ -0,0 +1,12 @@
+// Guess the output
+
+console.log(typeof(name));
+console.log(name);
+var name = "Jatin";
+sayMyName();
+function sayMyName() {
+    var name = "JatinBisht2308"
+    console.log(name);
+}
+sayMyName();
+console.log(name);

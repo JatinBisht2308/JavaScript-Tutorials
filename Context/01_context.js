7ae56be853bfeca8b38f919cbed5919f1c92760f
JatinBisht2308 <jatinbisht2308@gmail.com>
2022-04-09 10:45:22 +0530

    :fire:Context added

---
+++ Context/01_context.js
@@ -0,0 +1,9 @@
+// Context is the behaviour of js ,how js works, how the code execute in js etc etc.
+// In js function making after the function call also works
+sayHello();
+
+function sayHello()
+{
+    console.log("Hello");
+}
+

3254cd8cab1d92107ceb5713c4ba100837fd85be
JatinBisht2308 <jatinbisht2308@gmail.com>
2022-04-18 14:20:17 +0530

    :fire: loops added

--- Loops/01_loops.js
+++ Loops/01_loops.js
@@ -5,3 +5,8 @@ for (var i = 0;i<10;i++)
 }
 
 // learning loops concept using array
+const nameChars = ["J","A","T","I","N",123,false,"B","I","S","H","T"];
+for (let i = 0; i < nameChars.length; i++) {
+    if (typeof (nameChars[i]) === "string") continue;
+    console.log(nameChars[i]);
+}

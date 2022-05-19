aa6f3ef91dc60be9003706408b85a48c6f9f7d7b
JatinBisht2308 <jatinbisht2308@gmail.com>
2022-05-20 01:22:17 +0530

    :fire: changes made

--- Type_Casting/01_typecasting.js
+++ Type_Casting/01_typecasting.js
@@ -3,12 +3,13 @@
 // Some of the values that you must aware about(These are known as falsy values in js.)
 // 1)- undefined value -> if you dont give any value to an variable then it would be considered as the undefined data type 
 
-var x ;
+var x;
 console.log(typeof(x));
 
 
 // 2)- null -> null dataytype defines a specific value which repersents empty.
  var y = null;
+ console.log(typeof(y));
 if (y) {
     console.log("True");
 }
@@ -19,6 +20,7 @@ else{
 // But if we make a string as null then see it will run and give true
 
 var str = "null";
+console.log(typeof(str));
 if (str) {
     console.log("True");
 }

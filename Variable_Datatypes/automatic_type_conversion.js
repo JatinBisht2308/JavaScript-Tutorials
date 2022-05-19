fd621d23225e724880283b9b8f397a11665e9d4e
JatinBisht2308 <jatinbisht2308@gmail.com>
2022-05-20 00:33:34 +0530

    :fire: changes made

--- Variable_Datatypes/automatic_type_conversion.js
+++ Variable_Datatypes/automatic_type_conversion.js
@@ -2,6 +2,8 @@
  let a = 10; 
  let b = "20";
  let c = b - a;
+ let x = a + b;
+ console.log(x);
  console.log(c);
 //  except + all other operation will be treated as number dosent matter it is string or not
 let d = a * b;

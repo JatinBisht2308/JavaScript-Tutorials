3e0bab4c615a7581707a998456bbf56694c11e89
JatinBisht2308 <jatinbisht2308@gmail.com>
2022-05-21 01:58:44 +0530

    :fire: v2.0.0

---
+++ Array/full_array.js
@@ -0,0 +1,21 @@
+var arr = [1,2,3,4,5,6,67];
+var arr = new Array();
+var arr = new Array(5);
+var arr = new Array(1,2,3,4,5);
+
+var jatin = [1,2,3,4,5]
+jatin.shift();
+console.log(jatin);
+jatin.pop();
+console.log(jatin);
+jatin.unshift(1);
+console.log(jatin);
+jatin.push(5);
+console.log(jatin);
+console.log(jatin.reverse());
+jatin.sort();
+console.log(jatin);
+jatin.splice(1,3);
+console.log(jatin);
+console.log(jatin.join());
+console.log(jatin.toString());

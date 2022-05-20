b875fce3a2c3e275c254584fc04a3db74300cbff
JatinBisht2308 <jatinbisht2308@gmail.com>
2022-05-20 17:10:51 +0530

    :fire: color picker project added

--- DOM/DOM2/script.js
+++ DOM/DOM2/script.js
@@ -1,7 +1,8 @@
 // selects all the element first by query selector
 
 const red = document.querySelector(".red");
-const cyan = document.querySelector(".cyan");const violet = document.querySelector(".violet");
+const cyan = document.querySelector(".cyan");
+const violet = document.querySelector(".violet");
 const orange = document.querySelector(".orange");
 const pink = document.querySelector(".pink");
 
@@ -10,11 +11,11 @@ const center = document.querySelector(".center");
 
 // window object has lots of methods one of them is getComputedStyle used to select the css of a particular elemenmt 
 // console.log(window.getComputedStyle(red).backgroundColor);
-
+//  extracting color from the selectedElement 
 const getBgColor = (selectedElement) => {
    return window.getComputedStyle(selectedElement).backgroundColor;
 }
-
+// prints the value of the rgb color in the console inspect it
 console.log(getBgColor(violet));
 
 // evernt listeners and events 
@@ -29,7 +30,7 @@ const colorPicker = (element,color) => {
          center.style.backgroundColor = color;
      })
 };
-
+// getBgColor gives the actual rgb value color of the element we are passing so we directly used the function get bg to directly get the rgb value of a color and passed it to the colorPicker function
 colorPicker(red,getBgColor(red));
 colorPicker(violet,getBgColor(violet));
 colorPicker(cyan,getBgColor(cyan));

9d9e1f7bbc7737a569c063f42c71ded968cd0ddf
JatinBisht2308 <jatinbisht2308@gmail.com>
2022-05-21 14:04:54 +0530

    :fire: v2.0.0

---
+++ Objects/full_object.js
@@ -0,0 +1,12 @@
+var obj = {
+    name:"Jatin"
+};
+console.log(obj.name);
+obj.lastName = "Bisht";
+console.log(obj.lastName);
+console.log(obj);
+
+var arr = [10,20,30];
+arr["four"] = 40;
+console.log(arr);
+console.log(arr.length);

8f9de4724c782dbb54515167aca5722c06738b0a
JatinBisht2308 <jatinbisht2308@gmail.com>
2022-05-22 20:45:33 +0530

    :fire: v3.0.1

---
+++ Array/07_array.js
@@ -0,0 +1,13 @@
+// Array Destructuring
+const arr = ["val1","val2"];
+//  If we wanted to perform the below task in a shortcut way then we use array destrucuturing
+let var1 = arr[0];
+let var2 = arr[1];
+console.log("var1: " , var1);
+console.log("var2: " , var2);
+
+
+// Shortcut method -> array destrucuturing
+let [v1,v2] = arr;
+console.log(v1);
+console.log(v2);

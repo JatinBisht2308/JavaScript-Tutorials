987bdb383d1c17eb7c8227894f4ecd8f1a335911
JatinBisht2308 <jatinbisht2308@gmail.com>
2022-05-30 16:32:07 +0530

    :fire: more added

---
+++ Promises/03_promises.js
@@ -0,0 +1,37 @@
+// shop which only serves coffee
+
+// function for placing order will return a promise
+let takeOrder = (order) => {
+    return new Promise( (resolve,reject) => {
+       if(order=="coffee")
+       {
+           resolve("Oder Placed");
+       }
+       else{
+           reject("Sorry we only serve coffee")
+       }
+    })
+}
+
+// makeCoffee funciton (if the order is coffee then we have to prepare it)
+let makeCoffee = (order,gender) => {
+    // once the order is placed then we cannot reject it because it will pay us
+    return new Promise( (resolve) => {
+        if(gender=="female")
+        {
+            resolve("Here's your coffee mam")
+        }
+        else{
+            resolve(`Here's your ${order} sir`)
+        }
+    })
+}
+
+
+// final processing of the coffe shop
+takeOrder('coffee').then((oderState)=> {
+      console.log(oderState);
+    //  returning that promise to the another object(object because promises are objects)
+      
+    }
+)

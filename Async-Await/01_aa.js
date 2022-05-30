6454304eb4b65d00519cb3b815011e4d52e52fa2
JatinBisht2308 <jatinbisht2308@gmail.com>
2022-05-30 17:00:29 +0530

    :fire: more added

--- Async-Await/01_aa.js
+++ Async-Await/01_aa.js
@@ -2,3 +2,9 @@
 
 // Async is nothing but just a better way to apply promises it reduces the amount of writing then then then in the code
 
+async function serveOrder()
+{
+    const receiveOrder = await placeOrder('coffee');
+    console.log(receiveOrder);
+    
+}

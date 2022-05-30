6d3bbec1553ae9aa314d8bcc8cffad8b7a56c815
JatinBisht2308 <jatinbisht2308@gmail.com>
2022-05-30 16:35:26 +0530

    :fire: more added

--- Promises/03_promises.js
+++ Promises/03_promises.js
@@ -32,6 +32,10 @@ let makeCoffee = (order,gender) => {
 takeOrder('coffee').then((oderState)=> {
       console.log(oderState);
     //  returning that promise to the another object(object because promises are objects)
-      
-    }
-)
+      let banaCoffee = makeCoffee('coffee');
+      return banaCoffee;
+    }).then(function (orderDone){
+        console.log(orderDone);
+    }).catch(function (err){
+        console.log(err);
+    })

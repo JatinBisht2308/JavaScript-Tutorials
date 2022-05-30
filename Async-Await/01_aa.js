b492043629dd30e25902232f6c278034584d0aa2
JatinBisht2308 <jatinbisht2308@gmail.com>
2022-05-30 16:47:33 +0530

    :fire: more added

--- Async-Await/01_aa.js
+++ Async-Await/01_aa.js
@@ -1,2 +1,4 @@
 // coffee shop scenario with async and await go to promises03 to see the scenario with promises 
 
+// Async is nothing but just a better way to apply promises it reduces the amount of writing then then then in the code
+

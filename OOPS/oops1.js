e8faa042cb86ac21e9ae064a1e4cd51399e1d48a
Jatin <jatinbisht2308@gmail.com>
2024-01-31 11:14:07 +0530

    added the this keyword behaviour in node strict and non strict mode

--- OOPS/oops1.js
+++ OOPS/oops1.js
@@ -1,3 +1,40 @@
 // OOPS 1
 // this keyword:- behaves differently in browser and in node.js
 // further there are two modes strict and non-strict mode
+// Now we will only see for the this keyword in node js non  strict mode
+
+// check for the strict mode inside node by the below code.
+'use strict'
+// 1: normally it gives an empty object.
+console.log("normal console log",this);
+
+// 2: iniside a function it gives us an global object.
+function showThis()
+{
+    console.log("inside a function",this);
+}
+showThis();
+
+// 3: inside an object it gives us the object itself.
+
+let obj = {
+    name:"Jatin",
+    func: function(){
+        console.log("calling a function which is inside an object",this);
+    }
+}
+obj.func();
+
+// 4: inside an nested function in a function which is inside an object
+
+let obj2 = {
+    name:"Jatin nested",
+    func2: function(){
+        function nestedThis()
+        {
+            console.log("inside a nested function which is inside and object",this);
+        }
+        nestedThis();
+    }
+}
+obj2.func2();
